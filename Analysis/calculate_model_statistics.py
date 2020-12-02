2d29d09ba406721ceca1d8c2325668a72163d3af
kaarel.tark <kaareltark@gmail.com>
2020-12-02 21:41:33 +0200

    added R2 to model statistics script

--- Analysis/calculate_model_statistics.py
+++ Analysis/calculate_model_statistics.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import numpy as np
 ###Prints out RMSE, MAE for both individual measurements and also next day total measurements
 # input:
@@ -7,21 +7,22 @@ import numpy as np
 # - predictions on the test data
 def calculate_RMSE_MSE(test_data, predict_column, test_predictions):
     #INDIVIDUAL PERIODS PREDICTIONS
-    RMSE_tm = mean_squared_error(test_data[predict_column], test_predictions)
+    RMSE_tm = np.sqrt(mean_squared_error(test_data[predict_column], test_predictions))
     MAE_tm = mean_absolute_error(test_data[predict_column], test_predictions)
-
+    R2_tm = r2_score(test_data[predict_column], test_predictions) #uniform average
     #GROUPED BY DAY PREDICATIONS SCORES
     test_data['PREDICTED_DC_POWER'] = test_predictions
     grouped_test_data_target = test_data.groupby(['DAY_OF_YEAR'])[['DC_POWER', 'PREDICTED_DC_POWER']].agg({'sum'}).reset_index()
-    grouped_test_data_target = grouped_test_data_target.set_axis(['DAY_OF_YEAR','DC_POWER_SUM','PREDICTED_DC_POWER_SUM'], axis = 1, inplace=False)
-    #print(grouped_test_data_target)
+    grouped_test_data_target = grouped_test_data_target.set_axis(['DAY_OF_YEAR','DC_POWER_SUM','PREDICTED_DC_POWER_SUM'], axis = 1)
+    print(grouped_test_data_target)
 
-    RMSE_daily = mean_squared_error(grouped_test_data_target['DC_POWER_SUM'], grouped_test_data_target['PREDICTED_DC_POWER_SUM'])
+    RMSE_daily = np.sqrt(mean_squared_error(grouped_test_data_target['DC_POWER_SUM'], grouped_test_data_target['PREDICTED_DC_POWER_SUM']))
     MAE_daily = mean_absolute_error(grouped_test_data_target['DC_POWER_SUM'], grouped_test_data_target['PREDICTED_DC_POWER_SUM'])
+    R2_daily = r2_score(grouped_test_data_target['DC_POWER_SUM'], grouped_test_data_target['PREDICTED_DC_POWER_SUM'])
 
     print('daily predictions:')
-    print(f' RMSE - {np.round(RMSE_daily,0)}; MAE - {np.round(MAE_daily,0)}')
+    print(f' RMSE:{np.round(RMSE_daily,0)}; MAE:{np.round(MAE_daily,0)}; R2:{np.round(R2_daily,5)}')
 
     print('individual measurements:')
-    print(f' RMSE - {np.round(RMSE_tm,0)}; MAE - {np.round(MAE_tm,0)}')
+    print(f' RMSE:{np.round(RMSE_tm,0)}; MAE:{np.round(MAE_tm,0)}; R2:{np.round(R2_tm,5)}')
 
--- Results.md
+++ Results.md
@@ -12,7 +12,7 @@ Function to calculate the Daily and 15 min scores: [calculate_model_statistics.p
 ### Plant 1
 |Method|Link to Source|Comments-parameters|SCORE day (DC_POWER)|SCORE 15min (DC_POWER)|Status
 |---------|------------|------|-----|---|---|
-|Baseline - Plant 1|[link](/Analysis/01_running_mean.ipynb)|Last 3 days average for each inverter|RMSE - 4526646363222.0; MAE - 1361016.0|RMSE - 4469136.0; MAE - 940.0|Completed
+|Baseline - Plant 1|[link](/Analysis/01_running_mean.ipynb)|Last 3 days average for each inverter|RMSE - 2127592.0; MAE - 1361016.0|RMSE - 2114.0; MAE - 940.0|Completed
 |Linear regression - PLant 1|[link](/Analysis/xyz.ipynb)|TBD|TBD|TBD|Under work
 |Logistic regression - Plant 1|[link](/Analysis/xyz.ipynb)|TBD|TBD|TBD|Under work
 |Random Forest - Plant 1|[link](/Analysis/xyz.ipynb)|TBD|TBD|TBD|Under work
@@ -21,7 +21,7 @@ Function to calculate the Daily and 15 min scores: [calculate_model_statistics.p
 ### Plant 2
 |Method|Link to Source|Comments-parameters|SCORE day (DC_POWER)|SCORE 15min (DC_POWER)|Status
 |---------|------------|------|-----|---|---|
-|Baseline - Plant 2|[link](/Analysis/01_running_mean.ipynb)|Last 3 days average for each inverter|RMSE - 34958148529.0; MAE - 133949.0|RMSE - 57728.0; MAE - 132.0|Completed
+|Baseline - Plant 2|[link](/Analysis/01_running_mean.ipynb)|Last 3 days average for each inverter|RMSE - 186971.0; MAE - 133949.0|RMSE - 240.0; MAE - 132.0|Completed
 |Linear regression - PLant 2|[link](/Analysis/xyz.ipynb)|TBD|TBD|TBD|Under work
 |Logistic regression - Plant 2|[link](/Analysis/xyz.ipynb)|TBD|TBD|TBD|Under work
 |Random Forest - Plant 2|[link](/Analysis/xyz.ipynb)|TBD|TBD|TBD|Under work
